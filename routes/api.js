82791fcc410e35582901a12c154c3ea6dad89bfb
Natalalia <n.olmo.megias@gmail.com>
2019-04-23 12:06:45 +0100

    topic router file created

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,12 @@
-const apiRouter = require('express').Router();
-const { methodNotAllowed } = require('../errors');
+const apiRouter = require("express").Router();
+const topicsRouter = require("./topics_router");
+const { methodNotAllowed } = require("../errors");
 
 apiRouter
-  .route('/')
+  .route("/")
   .get((req, res) => res.send({ ok: true }))
   .all(methodNotAllowed);
+
+apiRouter.use("/topics", topicsRouter);
 
 module.exports = apiRouter;
