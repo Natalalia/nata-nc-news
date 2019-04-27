8a77beb5c237fbf3d8143eef033d4ae24fdccccf
Natalalia <n.olmo.megias@gmail.com>
2019-04-27 21:09:12 +0100

    topics and users seeding at once done

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -12,59 +12,37 @@ const {
 } = require("../../utils/formating-functions");
 
 exports.seed = (knex, Promise) => {
-  return (
-    knex.migrate
-      .rollback()
-      .then(() => knex.migrate.latest())
-      /*
-      .then(() => {
-        return Promise.all([topicsData, usersData]).then(([topics, users]) => {
-          return knex
-            .insert(topics)
-            .into("topics")
-            .returning("*")
-            .insert(users)
-            .into("users")
-            .returning("*");
-        });
-      })
-      */
+  return knex.migrate
+    .rollback()
+    .then(() => knex.migrate.latest())
+    .then(() => {
+      const topics = knex("topics")
+        .insert(topicsData)
+        .returning("*");
+      const users = knex("users")
+        .insert(usersData)
+        .returning("*");
+      return Promise.all([topics, users]);
+    })
+    .then(() => {
+      const newArticlesDate = newFormatDate(articlesData);
+      return knex("articles")
+        .insert(newArticlesDate)
+        .returning("*");
+    })
+    .then(articlesRow => {
+      const newCommentsDate = newFormatDate(commentsData);
+      const newKeyComments = changeKey(newCommentsDate, "created_by", "author");
+      const commentsRef = createObjectRef(articlesRow, "title", "article_id");
+      const formatedComments = formatData(
+        newKeyComments,
+        commentsRef,
+        "article_id",
+        "belongs_to"
+      );
 
-      .then(() => {
-        return knex("topics")
-          .insert(topicsData)
-          .returning("*");
-      })
-      .then(() => {
-        return knex("users")
-          .insert(usersData)
-          .returning("*");
-      })
-
-      .then(() => {
-        const newArticlesDate = newFormatDate(articlesData);
-        return knex("articles")
-          .insert(newArticlesDate)
-          .returning("*");
-      })
-      .then(articlesRow => {
-        const newCommentsDate = newFormatDate(commentsData);
-        const newKeyComments = changeKey(
-          newCommentsDate,
-          "created_by",
-          "author"
-        );
-        const commentsRef = createObjectRef(articlesRow, "title", "article_id");
-        const formatedComments = formatData(
-          newKeyComments,
-          commentsRef,
-          "article_id",
-          "belongs_to"
-        );
-
-        return knex("comments")
-          .insert(formatedComments)
-          .returning("*");
-      })
-  );
+      return knex("comments")
+        .insert(formatedComments)
+        .returning("*");
+    });
 };
